4d351053ac295449ae619026f0fbd83d724a2a73
Edward He <ehe058@uottawa.ca>
2024-03-17 15:23:24 -0400

    login page update

--- Lab8/my-app/src/App.js
+++ Lab8/my-app/src/App.js
@@ -1,14 +1,27 @@
-import './App.css';
-import LoginPage from './LoginPage'
+// App.js
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './components/Login/Loginpage';
+import UserProfile from './components/UserProfile/UserProfile';
 
 function App() {
   return (
-    <div className="App">
-
-      <LoginPage/>
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/user-profile" element={<UserProfile 
+          name="Edward He" 
+          age={30} 
+          job="Software Developer" 
+          company="Tech Innovations" 
+          image="/john.jpg" />} 
+        />
+        {/* Add other routes as needed */}
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
