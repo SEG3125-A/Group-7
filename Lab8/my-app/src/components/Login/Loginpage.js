c288686aa74922c71f29892d80fa557f62f9668e
Eeshaan Patel <epate039@uottawa.ca>
2024-03-24 17:26:55 -0400

    background

--- Lab8/my-app/src/components/Login/Loginpage.js
+++ Lab8/my-app/src/components/Login/Loginpage.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate, Link} from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import './Loginpage.css';
 
 const LoginPage = () => {
@@ -21,35 +21,37 @@ const LoginPage = () => {
   };
 
   return (
-    <div>
-      <h1>Welcome back to Roast Resume!</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">Email</label>
-        <input 
-          type="email" 
-          id="email" 
-          name="email"
-          value={email} 
-          onChange={handleChange} 
-          placeholder='Enter your email' 
-          required 
-        />
-        <br/>
-        <label htmlFor="password">Password</label>
-        <input 
-          type="password" 
-          id="password" 
-          name="password"
-          value={password} 
-          onChange={handleChange} 
-          placeholder='Enter your password' 
-          required 
-        />
-        <br/>
-        {error && <p className="error">{error}</p>}
-        <button type="submit">LOGIN</button>
-      </form>
-      <p>Don't have a account yet?<Link to="sign-up">Sign Up!</Link></p>
+    <div className="background-container">
+      <div className="form-container">
+        <h1>Welcome back to Roast Resume!</h1>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={handleChange}
+            placeholder='Enter your email'
+            required
+          />
+          <br/>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={handleChange}
+            placeholder='Enter your password'
+            required
+          />
+          <br/>
+          {error && <p className="error">{error}</p>}
+          <button type="submit">LOGIN</button>
+        </form>
+        <p>Don't have an account yet?<Link to="sign-up">Sign Up!</Link></p>
+      </div>
     </div>
   );
 }
--- Lab8/my-app/src/components/Signup/Signup.js
+++ Lab8/my-app/src/components/Signup/Signup.js
@@ -25,78 +25,80 @@ function Signup() {
   };
 
   return (
-    <div>
-    <h1>Welcome to Resume Roast</h1>
-      <h2>Sign Up</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>
-            Name:
-            <input
-              type="text"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-            />
-          </label>
+    <div className="background-container">
+      <div className="form-container">
+        <h1>How bad is your Resume</h1>
+          <h2>Sign Up</h2>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label>
+                Name:
+                <input
+                  type="text"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleChange}
+                />
+              </label>
+            </div>
+            <div>
+              <label>
+                Age:
+                <input
+                  type="number"
+                  name="age"
+                  value={formData.age}
+                  onChange={handleChange}
+                />
+              </label>
+            </div>
+            <div>
+              <label>
+                Job:
+                <input
+                  type="text"
+                  name="job"
+                  value={formData.job}
+                  onChange={handleChange}
+                />
+              </label>
+            </div>
+            <div>
+              <label>
+                Company:
+                <input
+                  type="text"
+                  name="company"
+                  value={formData.company}
+                  onChange={handleChange}
+                />
+              </label>
+            </div>
+            <div>
+              <label>
+                Email:
+                <input
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                />
+              </label>
+            </div>
+            <div>
+              <label>
+                Password:
+                <input
+                  type="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                />
+              </label>
+            </div>
+            <button type="submit">Sign Up</button>
+          </form>
         </div>
-        <div>
-          <label>
-            Age:
-            <input
-              type="number"
-              name="age"
-              value={formData.age}
-              onChange={handleChange}
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            Job:
-            <input
-              type="text"
-              name="job"
-              value={formData.job}
-              onChange={handleChange}
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            Company:
-            <input
-              type="text"
-              name="company"
-              value={formData.company}
-              onChange={handleChange}
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            Email:
-            <input
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            Password:
-            <input
-              type="password"
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-            />
-          </label>
-        </div>
-        <button type="submit">Sign Up</button>
-      </form>
     </div>
   );
 }
